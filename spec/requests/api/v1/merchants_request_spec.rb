2ac8e8ce86d1557fd3af8a7adfa83bff78b2607d
vjp888 <40582072+Vjp888@users.noreply.github.com>
2019-04-30 12:09:49 -0600

    Creates tests for basic api calls for merchants

---
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+describe 'Merchants API' do
+  it 'sends a list of merchants' do
+    create_list(:merchant, 3)
+
+    get '/api/v1/merchants.json'
+
+    expect(response).to be_successful
+
+    merchants = JSON.parse(response.body)
+
+    expect(merchants.count).to eq(3)
+  end
+
+  it 'finds based on given params' do
+    create(:merchant, name: 'bob')
+    create(:merchant, name: 'joe')
+    create(:merchant, name: 'steve')
+
+    get '/api/v1/merchants/find?name=steve'
+
+    expect(response).to be_successful
+
+    merchant = JSON.parse(response.body)
+
+    expect(merchant["name"]).to eq("steve")
+  end
+
+  it 'finds all based on given params' do
+    create(:merchant, name: 'bob')
+    create(:merchant, name: 'joe')
+    create(:merchant, name: 'steve')
+
+    get '/api/v1/merchants/find_all?name=steve'
+
+    expect(response).to be_successful
+
+    merchant = JSON.parse(response.body)
+
+    expect(merchant.count).to eq(1)
+  end
+end
